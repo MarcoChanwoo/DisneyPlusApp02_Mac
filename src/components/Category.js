b3e966ba23e75e57a699889bbfcfe78b4172bd29
Marco <cwhong8087@gmail.com>
2023-10-08 11:05:41 +0900

    updated

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,7 +1,31 @@
 import React from "react";
+import styled from "styled-components";
 
 const Category = () => {
-    return <div>Category</div>;
+    return (
+        <Container>
+            <Wrap>
+                <img src="/images/viewers-disney.png" alt="" />
+                <video autoPlay loop muted>
+                    <source src="/videos/disney.mp4" type="video/mp4" />
+                </video>
+            </Wrap>
+        </Container>
+    );
 };
 
 export default Category;
+
+const Container = styled.div`
+    margin-top: 30px;
+    padding: 30px 0px 26px;
+    display: grid;
+    gap: 25px;
+    grid-template-columns: repeat(5, 1fr);
+
+    @media (max-width: 768px) {
+        grid-template-columns: repeat(1, 1fr);
+    }
+`;
+
+const Wrap = styled.div``;
