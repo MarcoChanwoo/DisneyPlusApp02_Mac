759ba3f530eb5866a8afce18d222077d9f1ba9e0
Marco <cwhong8087@gmail.com>
2023-10-08 12:12:49 +0900

    updated

--- src/components/Category.js
+++ src/components/Category.js
@@ -5,9 +5,36 @@ const Category = () => {
     return (
         <Container>
             <Wrap>
-                <img src="/images/viewers-disney.png" alt="" />
+                <img src="/images/viewers-disney.png" alt="disney" />
                 <video autoPlay loop muted>
                     <source src="/videos/disney.mp4" type="video/mp4" />
+                </video>
+            </Wrap>
+            <Wrap>
+                <img src="/images/viewers-marvel.png" alt="marvel" />
+                <video autoPlay loop muted>
+                    <source src="/videos/marvel.mp4" type="video/mp4" />
+                </video>
+            </Wrap>
+            <Wrap>
+                <img src="/images/viewers-pixar.png" alt="pixar" />
+                <video autoPlay loop muted>
+                    <source src="/videos/pixar.mp4" type="video/mp4" />
+                </video>
+            </Wrap>
+            <Wrap>
+                <img src="/images/viewers-starwars.png" alt="starwars" />
+                <video autoPlay loop muted>
+                    <source src="/videos/star-wars.mp4" type="video/mp4" />
+                </video>
+            </Wrap>
+            <Wrap>
+                <img src="/images/viewers-national.png" alt="national" />
+                <video autoPlay loop muted>
+                    <source
+                        src="/videos/national-geographic.mp4"
+                        type="video/mp4"
+                    />
                 </video>
             </Wrap>
         </Container>
