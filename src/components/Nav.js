ef6ef9144f0ab0c801e28157b7b7688bf4507b90
Marco <cwhong8087@gmail.com>
2023-10-09 12:37:03 +0900

    updated

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 
 // 스크롤 시 NavBar 색깔 변경 -> NavWrapper에서 show={show}를 설정해야 최종 적용됨
 const Nav = () => {
     const [show, setShow] = useState(false);
+    const { pathname } = useLocation();
     useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if (window.scrollY > 50) {
-                setShow(true);
-            } else {
-                setShow(false);
-            }
-        });
+        window.addEventListener("scroll", handleScroll);
         return () => {
-            window.removeEventListener("scroll", () => {});
+            window.removeEventListener("scroll", handleScroll);
         };
     }, []);
+
+    const handleScroll = () => {
+        if (window.scrollY > 50) {
+            setShow(true);
+        } else {
+            setShow(false);
+        }
+    };
 
     return (
         <NavWrapper show={show}>
@@ -26,11 +30,25 @@ const Nav = () => {
                     onClick={() => (window.location.href = "/")}
                 />
             </Logo>
+
+            {pathname === "/" ? (
+                <Login>Login</Login>
+            ) : (
+                <Input
+                    className="nav_input"
+                    type="text"
+                    placeholder="검색해주세요."
+                />
+            )}
         </NavWrapper>
     );
 };
 
 export default Nav;
+
+const Login = styled.a``;
+
+const Input = styled.input``;
 
 const NavWrapper = styled.nav`
     position: fixed;
