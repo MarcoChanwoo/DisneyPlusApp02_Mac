c01fbfb850c474a567a1a0b480b4bf66a28dbf2b
Marco <cwhong8087@gmail.com>
2023-10-10 10:21:34 +0900

    updated

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -58,7 +58,22 @@ const LogoOne = styled.img`
     width: 100%;
 `;
 
-const SignUpLink = styled.a``;
+const SignUpLink = styled.a`
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: #0063e5;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1.5px;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transparent;
+    border-radius: 4px;
+
+    &:hover {
+        background-color: #0483ee;
+    }
+`;
 
 const Description = styled.p``;
 
