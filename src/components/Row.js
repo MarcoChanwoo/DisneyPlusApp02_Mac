c153b0d561d6006f17d2a7c43ec8bd709d9a9243
Marco <cwhong8087@gmail.com>
2023-10-11 12:53:19 +0900

    updated

--- src/components/Row.js
+++ src/components/Row.js
@@ -69,7 +69,7 @@ const Row = ({ title, id, fetchUrl }) => {
                                 <img
                                     key={movie.id}
                                     // className="row_poster"
-                                    src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                                    src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
                                     alt={movie.name}
                                     onClick={() => handleClick(movie)}
                                 />
