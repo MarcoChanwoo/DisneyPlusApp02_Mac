94c8858f8f0ff5e77b5b5e1134fd3f00702f2f45
Marco <cwhong8087@gmail.com>
2023-10-11 11:35:06 +0900

    updated

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -73,19 +73,33 @@ const Nav = () => {
             {pathname === "/" ? (
                 <Login onClick={handleAuth}>Login</Login>
             ) : (
-                <Input
-                    value={searchValue}
-                    onChange={handleChange}
-                    className="nav_input"
-                    type="text"
-                    placeholder="검색해주세요."
-                />
+                <>
+                    <Input
+                        value={searchValue}
+                        onChange={handleChange}
+                        className="nav_input"
+                        type="text"
+                        placeholder="검색해주세요."
+                    />
+                    <SignOut>
+                        <UserImg />
+                        <DropDown>
+                            <span>Sign Out</span>
+                        </DropDown>
+                    </SignOut>
+                </>
             )}
         </NavWrapper>
     );
 };
 
 export default Nav;
+
+const SignOut = styled.div``;
+
+const UserImg = styled.div``;
+
+const DropDown = styled.div``;
 
 const Login = styled.a`
     background-color: rgba(0, 0, 0, 0.6);
